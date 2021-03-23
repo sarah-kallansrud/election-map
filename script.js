1141fbedea27bcc34915b716f5eb4d0a834967b9
sarah_k <sarahkallansrud@gmail.com>
2021-03-23 13:40:01 -0500

    Assigned colors to cadidates and added color change on mouseover function

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-var makeCandidate = function(individual)
+var makeCandidate = function(individual, partyColor)
 {
   var politician = {};
   politician.name = individual;
   politician.electionResults = null;
   politician.totalVotes = 0;
+  politician.partyColor = partyColor;
   
   politician.announceCandidate = function()
   {
@@ -28,8 +29,8 @@ var makeCandidate = function(individual)
   
 };
 
-var candidate1 = makeCandidate("Loren Peterson");
-var candidate2 = makeCandidate("Blanca Ray");
+var candidate1 = makeCandidate("Loren Peterson", [245, 141, 136]);
+var candidate2 = makeCandidate("Blanca Ray", [132, 17, 11]);
 var winner = "?";
 
 candidate1.electionResults = [5, 1, 7, 2, 33, 6, 4, 2, 1, 14, 8, 3, 1, 11, 11, 0, 5, 3, 3, 3, 7, 4, 8, 9, 3, 7, 2, 2, 4, 2, 8, 3, 15, 15,2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 3, 2, 11, 1, 3, 7, 2];
@@ -45,6 +46,33 @@ candidate2.electionResults[4] = 38;
 
 candidate1.electionResults[43] = 11;
 candidate2.electionResults[43] = 27;
+
+var setStateResults = function(state){
+ 
+    theStates[state].winner = null;
+ 
+    if (candidate1.electionResults[state] > candidate2.electionResults[state]) {
+        
+        theStates[state].winner = candidate1; //set winner to the candidate object, not the candidate's name this time
+ 
+    } 
+  
+  else if (candidate1.electionResults[state] < candidate2.electionResults[state]) {
+ 
+        theStates[state].winner = candidate2;
+ 
+    }
+  
+  var stateWinner = theStates[state].winner;
+  
+  if (theStates[state].winner !== null) {
+    theStates[state].rgbColor = stateWinner.partyColor;
+  }
+  
+  else {
+    theStates[state].rgbColor = [11,32,57];
+  }
+}
 
 console.log(candidate1.electionResults);
 console.log(candidate2.electionResults);
@@ -63,6 +91,11 @@ else {
   winner = "Draw."
 }
 console.log("The new President is " + winner + "!!!!!!!!");
+
+
+console.log("Loren's color is: " + candidate1.partyColor);
+console.log("Blanca's color is: " + candidate2.partyColor);
+
 
     
 
